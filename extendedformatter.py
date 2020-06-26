5fe5958e8a91d4714d87e38e810c429607de2a0e
erosert <erosert@macbookair.local>
2020-06-26 16:53:05 +0200

    capitalized formatter modifiers

--- extendedformatter.py
+++ extendedformatter.py
@@ -5,13 +5,13 @@ from string import Formatter
 class ExtendedFormatter(Formatter):
     """An extended format string formatter
 
-    Formatter with extended conversion symbol
+    Formatter with extended conversion symbols '!U' (uppercase string) and '!L' (lowercase string).
     """
     def convert_field(self, value, conversion):
-        """ Extend conversion symbol
+        """ Extend conversion symbols
         Following additional symbol has been added
-        * l: convert to string and low case
-        * u: convert to string and up case
+        * L: convert to string and lower case
+        * U: convert to string and upper case
 
         default are:
         * s: convert with str()
@@ -19,9 +19,9 @@ class ExtendedFormatter(Formatter):
         * a: convert with ascii()
         """
 
-        if conversion == "u":
+        if conversion == "U":
             return str(value).upper()
-        elif conversion == "l":
+        elif conversion == "L":
             return str(value).lower()
         # Do the default conversion or raise error if no matching conversion found
         super(ExtendedFormatter, self).convert_field(value, conversion)
